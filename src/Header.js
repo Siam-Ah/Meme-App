846adc95bae4c15f830626a73581813df9e0914a
Siam <sahm0036@student.monash.edu>
2024-08-05 11:16:55 +1000

    project deploy

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header className="navbar">
+            <img src="./images/troll-face.png" />
+            <h2>Meme Generator</h2>
+            <h4>React Course - Project 3</h4>
+        </header>
+    )
+}
